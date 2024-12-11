b5cbcfdb993cb018d32a4f499910ac7cf8fbe3cf
Cherryello <felicedarioceriello@gmail.com>
2024-12-11 13:03:45 +0100

    Initial Commit

---
+++ bravebird/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bravebird
+//
+//  Created by Felice Dario Ceriello on 11/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bravebird/bravebirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  bravebirdApp.swift
+//  bravebird
+//
+//  Created by Felice Dario Ceriello on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct bravebirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
